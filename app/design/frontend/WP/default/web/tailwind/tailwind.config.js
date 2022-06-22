74558c76f0db00da1afee3452dfaa5f1da8743cb
huync <huy3001@gmail.com>
2022-06-22 11:28:10 +0700

    Create default theme and build header

---
+++ app/design/frontend/WP/default/web/tailwind/tailwind.config.js
@@ -0,0 +1,59 @@
+const { spacing } = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    theme: {
+        extend: {
+            screens: {
+                'sm': '640px',
+                // => @media (min-width: 640px) { ... }
+                'md': '768px',
+                // => @media (min-width: 768px) { ... }
+                'lg': '1024px',
+                // => @media (min-width: 1024px) { ... }
+                'xl': '1280px',
+                // => @media (min-width: 1280px) { ... }
+                '2xl': '1536px',
+                // => @media (min-width: 1536px) { ... }
+            },
+            backgroundImage: {
+                'header-wrapper': 'url(../images/bg-wrapper.png)'
+            },
+            colors: {
+                'carnaby-tan': '#583622',
+                'carrot-orange': '#ee7016',
+                'sambuca': '#3a2d25',
+                'sauvignon': '#f8f3ee',
+                'sorrell-brown': '#997b68',
+                'spicy-mix': '#84634f'                                                                                                            
+            },
+            fontFamily: {
+                'base': ['Roboto', 'sans-serif'],
+                'merri': ['Merriweather', 'serif']
+            },
+            fontSize: {
+                'full': '100%'
+            },
+            height: {
+                '13': '52px'
+            },
+            inset: {
+                '18': '4.5rem'
+            },
+            maxWidth: {
+                'container': '90rem'
+            },
+            width: {
+                '45': '180px'
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography')
+    ],
+    content: [
+        // this theme's phtml or js files
+        '../../**/*.{phtml, js}'
+    ]
+}
